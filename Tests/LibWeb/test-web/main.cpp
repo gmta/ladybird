0e1f63c737be75bd26f3b9b3cede4ff7f4fb95bf
Shannon Booth <shannon@serenityos.org>
2025-12-31 16:30:01 +0100

    LibURL+Elsewhere: Consider file:// origins opaque by default
    ...Except in test-web, where we rely on the existing
    behaviour to, for example, run localStorage tests which
    would otherwise be disabled. For test purposes, allow
    overriding this behaviour with a commandline flag.
    
    This aligns our behaviour closer to other browsers, which
    _mostly_ consider file scheme URLs as opaque.

--- Tests/LibWeb/test-web/main.cpp
+++ Tests/LibWeb/test-web/main.cpp
@@ -184,6 +184,7 @@ static void render_live_display()
 
 static RefPtr<Core::Promise<Empty>> s_all_tests_complete;
 static Vector<ByteString> s_skipped_tests;
+static Vector<ByteString> s_depends_on_non_opaque_file_origins_tests;
 static HashMap<WebView::ViewImplementation const*, Test*> s_test_by_view;
 
 struct ViewOutputCapture {
@@ -304,6 +305,9 @@ static ErrorOr<void> load_test_config(StringView test_root_path)
         if (group == "Skipped"sv) {
             for (auto& key : config->keys(group))
                 s_skipped_tests.append(TRY(FileSystem::real_path(LexicalPath::join(test_root_path, key).string())));
+        } else if (group == "DependsOnTupleFileOrigins"sv) {
+            for (auto& key : config->keys(group))
+                s_depends_on_non_opaque_file_origins_tests.append(TRY(FileSystem::real_path(LexicalPath::join(test_root_path, key).string())));
         } else {
             warnln("Unknown group '{}' in config {}", group, config_path);
         }
@@ -1132,7 +1136,8 @@ static ErrorOr<int> run_tests(Core::AnonymousBuffer const& theme, Web::DevicePix
             }
 
             Core::deferred_invoke([&]() mutable {
-                if (s_skipped_tests.contains_slow(test.input_path))
+                if (s_skipped_tests.contains_slow(test.input_path)
+                    || (app.file_scheme_urls_have_opaque_origins && s_depends_on_non_opaque_file_origins_tests.contains_slow(test.input_path)))
                     view->on_test_complete({ test, TestResult::Skipped });
                 else
                     run_test(*view, test, app);
